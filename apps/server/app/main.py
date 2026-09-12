c13311515e4fb86ca2f1364beda1e1e0759e62ab
Lincoln <lincolnborsoimoreira@gmail.com>
2026-09-12 18:01:44 -0300

    refactor: arrumando a formatacao do codigo com o prettier e ruff

--- apps/server/app/main.py
+++ apps/server/app/main.py
@@ -1,15 +1,14 @@
-import os
-from fastapi import FastAPI
 import inngest.fast_api
 from dotenv import load_dotenv
+from fastapi import FastAPI
+
+from app.modules.support_agents.inngest.client import inngest_client
+from app.modules.support_agents.inngest.functions import support_agent_inngest_functions
+from app.routes import api_router
+from app.shared.database import check_database_connection
 
 load_dotenv()
 
-from app.routes import api_router
-from app.modules.support_agents.inngest.client import inngest_client
-from app.modules.support_agents.inngest.functions import support_agent_inngest_functions
-
-from app.shared.database import check_database_connection
 
 app = FastAPI(title="API 5 Semestre")
 
@@ -20,6 +19,7 @@ inngest.fast_api.serve(
 )
 
 app.include_router(api_router)
+
 
 @app.get("/health")
 def health_check() -> dict[str, str]:
