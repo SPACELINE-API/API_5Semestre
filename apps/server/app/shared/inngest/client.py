57999867540f9b58ef11413fcd94a1d80216b3c1
Dollar2006 <lincolnborsoimoreira@gmail.com>
2026-09-14 09:12:37 -0300

    style: formatacao de codigo seguindo o ci/cd

--- apps/server/app/shared/inngest/client.py
+++ apps/server/app/shared/inngest/client.py
@@ -2,6 +2,5 @@ import inngest
 import os
 
 inngest_client = inngest.Inngest(
-    app_id="api-5-semestre",
-    is_production=bool(os.getenv("INNGEST_SIGNING_KEY"))
+    app_id="api-5-semestre", is_production=bool(os.getenv("INNGEST_SIGNING_KEY"))
 )
