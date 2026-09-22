71d2e6725321bba1e30a5ede27633982a835a613
Leti-10 <leti.oli.lopes@gmail.com>
2026-09-21 22:32:35 -0300

    fix(responsividade): tela de clientes e os ajustadas como deveriam

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import {
 	SafeAreaProvider,
 	useSafeAreaInsets,
